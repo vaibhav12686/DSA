4d6e0469838e775f4742107974816b2119e01604
Vaibhav <vaibhavchandola7@gmail.com>
2026-09-16 18:10:29 +0530

    Factors of num

---
+++ Basic Math Logic/factors_of_num.py
@@ -0,0 +1,14 @@
+from math import sqrt
+
+def factors(num):
+
+    result = []
+    for i in range(1, int(sqrt(num) + 1)):
+        if num % 10 == 0:
+            result.append(i)
+            if num // i != i:
+                result.append(num // i)
+
+    return result
+
+print(factors(20))
